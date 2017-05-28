5b84f108bd0101509d3f671997a1fa693e656eb2
Florian Scheel <florian.scheel@zkrd.de>
2017-05-28 14:52:42 +0200

    Add the /hfc/{submissionId}/haplotypes implementation

--- src/main/java/org/nmdp/hfcus/controller/HFCurationApiController.java
+++ src/main/java/org/nmdp/hfcus/controller/HFCurationApiController.java
@@ -1,9 +1,11 @@
 package org.nmdp.hfcus.controller;
 
+import io.swagger.annotations.ApiParam;
 import io.swagger.api.HfcApi;
 import io.swagger.model.HFCurationListResponse;
 import io.swagger.model.HFCurationRequest;
 import io.swagger.model.HFCurationResponse;
+import io.swagger.model.HaplotypeFrequencyData;
 import org.nmdp.hfcus.dao.HFCurationRepository;
 import org.nmdp.hfcus.model.HFCuration;
 import org.springframework.beans.BeanUtils;
@@ -11,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
@@ -52,4 +55,12 @@ public class HFCurationApiController implements HfcApi{
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
+    @Override
+    public ResponseEntity<HaplotypeFrequencyData> hfcSubmissionIdHaplotypesGet(
+            @ApiParam(value = "The submission id",required=true ) @PathVariable("submissionId") String submissionId
+    ){
+        HFCuration curation = curationRepository.findOne(submissionId);
+        HaplotypeFrequencyData response = curation.getHaplotypeFrequencyData();
+        return ResponseEntity.ok(response);
+    }
 }

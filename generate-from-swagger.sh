3ed749c54f76fb3a20e4b4246a7d234f9d89aca5
pbashyal-nmdp <pbashyal@nmdp.org>
2018-02-02 12:12:07 -0600

    Use Swagger Codegen Plugin
    * Use Swagger Codegen Plugin to generate the server code from the spec file.
    * External swagger-generator not needed for server models/apis. Removed src/main/java/io from codebase.
    * Add docker-compose for local mysql

--- generate-from-swagger.sh
+++ generate-from-swagger.sh
@@ -4,4 +4,6 @@
 # swagger-codegen config-help -l java
 # Selective generation
 # https://github.com/swagger-api/swagger-codegen#selective-generation
-swagger-codegen generate -i curation-swagger-spec.yaml -l spring -o . -c swagger-config.json -Dmodels -Dapis
+# swagger-codegen generate -i curation-swagger-spec.yaml -l spring -o . -c swagger-config.json -Dmodels -Dapis
+
+echo "Not needed anymore. Maven will generate the sources when it builds the application."

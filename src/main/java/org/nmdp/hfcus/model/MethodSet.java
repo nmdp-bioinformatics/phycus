844222c06454d5de5ad49b163e69194a5f1aefcb
Florian Scheel <florian.scheel@zkrd.de>
2018-10-07 11:30:11 +0200

    Remove unnecessary definition of list only types

--- src/main/java/org/nmdp/hfcus/model/MethodSet.java
+++ src/main/java/org/nmdp/hfcus/model/MethodSet.java
@@ -16,9 +16,9 @@ public class MethodSet implements ICurationDataModel<MethodData> {
     }
 
     public MethodSet(MethodData swaggerObject) {
-        if (swaggerObject.getMethodList() != null && swaggerObject.getMethodList().getMethod() != null){
+        if (swaggerObject.getMethodList() != null){
             methodList = new ArrayList<>();
-            for (io.swagger.model.Method method : swaggerObject.getMethodList().getMethod() ){
+            for (io.swagger.model.Method method : swaggerObject.getMethodList() ){
                 methodList.add(new Method(method));
             }
             if (methodList.size() == 0){
@@ -61,9 +61,7 @@ public class MethodSet implements ICurationDataModel<MethodData> {
             for (Method method: methodList) {
                 methods.add(method.toSwaggerObject());
             }
-            MethodList list = new MethodList();
-            list.setMethod(methods);
-            data.setMethodList(list);
+            data.setMethodList(methods);
         }
         return data;
     }

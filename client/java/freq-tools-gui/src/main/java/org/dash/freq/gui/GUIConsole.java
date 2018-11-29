fc948e171917de0ac7e0696468969b3394047222
Katrina Eaton <kaeaton@chori.org>
2018-11-29 12:37:57 -0800

    file clean up

--- client/java/freq-tools-gui/src/main/java/org/dash/freq/gui/GUIConsole.java
+++ client/java/freq-tools-gui/src/main/java/org/dash/freq/gui/GUIConsole.java
@@ -15,41 +15,26 @@ import javax.swing.SwingUtilities;
  * @author katrinaeaton
  */
 public class GUIConsole {
-	public GUIConsole(){
+	public GUIConsole()
+	{
 		
 	}
-	//The run method is just a method I create to test the output. You can call it in the constructor methods after initComponents(); , or you can try to do a System.out.println(); in the about box action
  
-	public void run()
-	{
-		redirectSystemStreams();
-		System.out.println("THIS IS A DEMO FOR REDIRECTING OUTPUT TO GUI");
-
-
-		System.out.println("--------------------------------------------");
-
-		for(int i = 0; i < 30; i++)
-		{
-			System.out.print(Math.random()+"\t");
-			System.out.println("<<End");
-		}
-
-		System.out.println("--------------------------------------------");
-
-
-	}
-     
 //The following codes set where the text get redirected. In this case, jTextArea1    
-	private void updateTextArea(final String text) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
+	private void updateTextArea(final String text) 
+	{
+		SwingUtilities.invokeLater(new Runnable() 
+		{
+			public void run() 
+			{
 				GUIJFrame.outputTextArea.append(text);
 			}
 		});
 	}
  
-//Followings are The Methods that do the Redirect, you can simply Ignore them. 
-	protected void redirectSystemStreams() {
+	// invoked in the main method of GUIjFrame
+	protected void redirectSystemStreams() 
+	{
 		OutputStream out = new OutputStream() 
 		{
 			@Override

2e55b701c35e364656647e4b7bf5b781da7f3e97
Martin Maiers <mmaiers@nmdp.org>
2024-10-11 11:18:14 +0200

    fixed paths and parsing of config

--- hfx/mkhfx.py
+++ hfx/mkhfx.py
@@ -15,20 +15,20 @@ import os
 
 
 
-def csv_to_json(csv_file_path, json_file_path, schema_file_path, example_file_path):
-    # Load the example JSON to understand the structure of metaData
-    with open(example_file_path, 'r', encoding='utf-8') as example_file:
-        example_data = json.load(example_file)
+def csv_to_json(hapfreq_file_path, json_file_path, schema_file_path, metadata_file_path):
+    # Load the metadata JSON to understand the structure of metaData
+    with open(metadata_file_path, 'r', encoding='utf-8') as metadata_file:
+        metadata_data = json.load(metadata_file)
 
-    # Extract metaData from example JSON
-    meta_data = example_data["metaData"]
+    # Extract metaData from metadata JSON
+    meta_data = metadata_data["metaData"]
 
     # Initialize frequencyData list
     frequency_data = []
 
     # Read CSV file and convert rows to required format
-    with open(csv_file_path, mode='r', encoding='utf-8') as csv_file:
-        csv_reader = csv.reader(csv_file)
+    with open(hapfreq_file_path, mode='r', encoding='utf-8') as hapfreq_file:
+        csv_reader = csv.reader(hapfreq_file)
         for row in csv_reader:
             haplotype, frequency = row[0], float(row[1])
             frequency_data.append({
@@ -47,28 +47,28 @@ def csv_to_json(csv_file_path, json_file_path, schema_file_path, example_file_pa
         json.dump(final_json_data, json_file, indent=4)
 
 # File path defaults
-csv_file = '../data/US_CAU.freqs'
+hapfreq_file = '../data/US_CAU.freqs'
 json_file = './hfx.json'
 schema_file = './hfx.schema.json'
-example_file = './hfx.example.json'
+metadata_file = './hfx.metadata.json'
 
 parser = argparse.ArgumentParser(description='configfile')
-parser.add_argument('--conf', default='mkhfx.json')
+parser.add_argument('--conf', default='config.json')
 args = parser.parse_args()
 
 if os.path.exists(args.conf):
     with open(args.conf, 'rt') as f:
         config = json.load(f)
 
-if csv_file in config.keys() and config['csv_file']:
-    csv_file = config['csv_file']
-if config['json_file']:
+if 'hapfreq_file' in config.keys() and config['hapfreq_file']:
+    hapfreq_file = config['hapfreq_file']
+if 'json_file' in config.keys() and config['json_file']:
     json_file = json_file
-if config['schema_file']:
+if 'schema_file' in config.keys() and config['schema_file']:
     schema_file = config['schema_file']
-if config['example_file']:
-    example_file = config['example_file']
+if 'metadata_file' in config.keys() and config['metadata_file']:
+    metadata_file = config['metadata_file']
 
 # Convert CSV to JSON using the schema and example
-csv_to_json(csv_file, json_file, schema_file, example_file)
+csv_to_json(hapfreq_file, json_file, schema_file, metadata_file)
 
